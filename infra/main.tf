7b64b549a2c69b1899078c4741d69c870b1fafab
JonasNT30 <jonasntorres@hotmail.com>
2025-08-31 15:13:22 +0000

    feat(infra): final variables  correction

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,5 @@
-resource "ecs_cluster_name" "this" {
+# Definição do Cluster ECS
+resource "aws_ecs_cluster" "this" {
   name = format("%s-cluster", var.cluster_name)
 
   setting {
@@ -6,3 +7,70 @@ resource "ecs_cluster_name" "this" {
     value = "enabled"
   }
 }
+
+# Network Load Balancer (NLB)
+resource "aws_lb" "this" {
+  name               = format("%s-nlb", var.cluster_name)
+  subnets            = var.subnets_id
+  security_groups    = [aws_security_group.allow_inbound.id]
+  load_balancer_type = "network"
+
+  tags = {
+    Name = format("%s-nlb", var.cluster_name)
+  }
+}
+
+# NLB listener
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+# NLB target group
+resource "aws_lb_target_group" "this" {
+  name        = format("%s-tg", var.cluster_name)
+  port        = 80
+  protocol    = "TCP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  tags = {
+    Name = format("%s-tg", var.cluster_name)
+  }
+}
+
+# Grupo de segurança
+resource "aws_security_group" "allow_inbound" {
+  name        = format("%s-sg", var.cluster_name)
+  vpc_id      = var.vpc_id
+  description = "Allow inbound traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = format("%s-sg", var.cluster_name)
+  }
+}
+
+# Regras de entrada para o grupo de segurança
+resource "aws_vpc_security_group_ingress_rule" "web" {
+  security_group_id = aws_security_group.allow_inbound.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+  description       = "Acesso Web"
+}
+
+resource "aws_vpc
