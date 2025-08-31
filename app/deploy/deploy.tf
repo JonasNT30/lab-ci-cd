425ba5b3f2d054403171a96ca974af4aff2ab548
JonasNT30 <jonasntorres@hotmail.com>
2025-08-31 01:15:08 +0000

    fix(deploy): corrected subnet IDs

--- app/deploy/deploy.tf
+++ app/deploy/deploy.tf
@@ -1,40 +1,86 @@
-data "aws_lb_target_group" "this" {
-  name = "app-prod-tg"
-}
-
+# data.tf
 data "aws_security_groups" "this" {
   filter {
-    name   = "tag:Name"
-    values = ["app-prod-sg"]
+    name   = "group-name"
+    values = [var.security_group_name]
   }
 }
 
 data "aws_lb" "this" {
-  name = var.lb_name
+  name = var.nlb_name
+}
+
+data "aws_lb_target_group" "this" {
+  name = var.tg_name
+}
+
+
+# deploy.tf
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/ecs/${var.container_name}"
+  retention_in_days = 7
 }
 
 resource "aws_ecs_service" "this" {
-  name                          = "app-service"
-  task_definition               = "ci-cd-app"
-  cluster                       = var.cluster_name
-  desired_count                 = var.desired_count
-  launch_type                   = "FARGATE"
-  availability_zone_rebalancing = "ENABLED"
+  name            = var.ecs_service_name
+  cluster         = var.ecs_cluster_name
+  task_definition = var.task_definition_name
+  desired_count   = 3
+  launch_type     = "FARGATE"
+
   network_configuration {
-    subnets          = var.subnets_id
-    security_groups  = data.aws_security_groups.this.ids
+    subnets = [
+      "subnet-013d682169102afe0",
+      "subnet-076059ee3f9904a0d",
+      "subnet-07ed8aa5282321a33"
+    ]
     assign_public_ip = true
+    security_groups  = [data.aws_security_groups.this.ids[0]]
   }
 
   load_balancer {
-    target_group_arn = data.aws_lb_target_group.this.arn
-    container_name   = "ci-cd-app"
+    container_name   = var.container_name
     container_port   = 8000
+    target_group_arn = data.aws_lb_target_group.this.arn
   }
 
+  depends_on = [aws_cloudwatch_log_group.this]
 }
 
-resource "aws_cloudwatch_log_group" "this" {
-  name              = "/ecs/ci-cd-app"
-  retention_in_days = 7
+
+# variables.tf
+variable "container_name" {
+  type    = string
+  default = "ci-cd-app"
+}
+
+variable "ecs_service_name" {
+  type = string
+}
+
+variable "ecs_cluster_name" {
+  type = string
+}
+
+variable "task_definition_name" {
+  type = string
+}
+
+variable "security_group_name" {
+  type    = string
+  default = "sg-0e90f81d503fe74a9"
+}
+
+variable "nlb_name" {
+  type    = string
+  default = "app-prod-nlb"
+}
+
+variable "tg_name" {
+  type    = string
+  default = "app-prod-tg"
+}
+
+output "nlb_dns_name" {
+  value = data.aws_lb.this.dns_name
 }
